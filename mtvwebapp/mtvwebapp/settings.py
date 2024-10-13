52f6231d53e96d94b4d0d65036e280359187cdb7
dvar98 <varela13guerrero@gmail.com>
2024-10-12 20:04:25 -0500

    Update settings.py

--- mtvwebapp/mtvwebapp/settings.py
+++ mtvwebapp/mtvwebapp/settings.py
@@ -73,18 +73,13 @@ WSGI_APPLICATION = 'mtvwebapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hackathon',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
